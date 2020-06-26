82c839e185d0d2da59f83752e47c805d3cbdb315
cohenta2 <cohenta2@vcu.edu>
2020-06-26 11:56:28 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -135,12 +135,3 @@ create_table_queries = [songplay_table_create, user_table_create,
     song_table_create, artist_table_create, time_table_create]
 
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-
-"""
-INSERT INTO {table}
-    ({cols})
-VALUES
-    (%s, %s, %s, %s, %s)
-ON CONFLICT ({pkey}) 
-    DO NOTHING;
-"""
